b7ce8f93d9d90f35453e8e050926ff9bdd5827c5
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-06-20 00:03:51 +0200

    adding first example for login / index view

---
+++ UnsecureWebApp/Model/FormData/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnsecureWebApp.Model.FormData
+{
+
+    public class UserData
+    {
+
+        public string UserEmail { get; set; }
+
+        public string UserPassword { get; set; }
+
+    }
+
+}
--- UnsecureWebApp/Pages/Index.cshtml.cs
+++ UnsecureWebApp/Pages/Index.cshtml.cs
@@ -1,25 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using UnsecureWebApp.Model.Database;
+using UnsecureWebApp.Model.FormData;
 
 namespace UnsecureWebApp.Pages
 {
+
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> FLogger;
+        private readonly DbModel FDataBase;
+
+        [BindProperty]
+        public UserData Form { get; set; }
+
+        public IndexModel(ILogger<IndexModel> ALogger, DbModel ADataBase)
         {
-            _logger = logger;
+            FLogger   = ALogger;
+            FDataBase = ADataBase;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
 
+            ViewData["Info"] = "Please provide credentials.";
+
+            return Page();
+
         }
+
+        public IActionResult OnPost(UserData Form) 
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (IsUserAuthenticated(Form.UserEmail, Form.UserPassword))
+                {
+                    ViewData["Info"] = "Validated.";
+                }
+                else 
+                {
+                    ViewData["Info"] = "Incorrect login/password.";
+                }
+
+            }
+            else
+            {
+                ViewData["Info"] = "Model is invalid.";
+            }
+
+            return Page();
+
+        }
+
+        /// <summary>
+        /// Example of SQL Injection vulnerability. Never use such code!
+        /// </summary>
+        /// <param name="AEmail"></param>
+        /// <param name="APassword"></param>
+        /// <returns></returns>
+        private bool IsUserAuthenticated(string AEmail, string APassword) 
+        {
+
+            var Result = FDataBase.Users.FromSqlRaw("SELECT Id FROM dbo.Users WHERE EmailAddress = '" + AEmail + "' AND HashedPassword = '" + APassword + "'");
+
+            if (Result.Any())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
     }
+
 }
