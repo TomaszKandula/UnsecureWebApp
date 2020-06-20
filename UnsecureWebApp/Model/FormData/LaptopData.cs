346a4632adbd9c3b0a7bf43063c1e6c03c3fa6a3
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-06-20 18:55:48 +0200

    adding laptops list

---
+++ UnsecureWebApp/Model/FormData/LaptopData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnsecureWebApp.Model.FormData
+{
+    
+    public class LaptopData
+    {
+
+        public string Brand { get; set; }
+
+        public string SerialNo { get; set; }
+        
+        public int Userid { get; set; }
+
+    }
+
+}
---
+++ UnsecureWebApp/Pages/Laptops.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using UnsecureWebApp.Model.Database;
+using UnsecureWebApp.Model.FormData;
+
+namespace UnsecureWebApp.Pages
+{
+
+    public class LaptopsModel : PageModel
+    {
+
+        private readonly ILogger<LaptopsModel> FLogger;
+        private readonly DbModel FDataBase;
+
+        [BindProperty]
+        public List<LaptopData> Form { get; set; }
+
+        public LaptopsModel(ILogger<LaptopsModel> ALogger, DbModel ADataBase)
+        {
+            FLogger   = ALogger;
+            FDataBase = ADataBase;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string Brand)
+        {
+
+            Form = new List<LaptopData>();
+
+            if (!string.IsNullOrEmpty(Brand))
+            {
+
+                var Laptops = await ReturnLaptopsAsync(Brand);                              
+                
+                foreach (var Laptop in Laptops) 
+                {
+
+                    var LaptopData = new LaptopData()
+                    {
+                        Brand    = Laptop.Brand,
+                        SerialNo = Laptop.SerialNo,
+                        Userid   = Laptop.UserId
+                    };
+
+                    Form.Add(LaptopData);
+                
+                }
+            
+            }
+
+            return Page();
+
+        }
+
+        /// <summary>
+        /// Example of SQL Injection vulnerability. Never use such code!
+        /// </summary>
+        /// <param name="ABrand"></param>
+        /// <returns></returns>
+        private async Task<List<Laptops>> ReturnLaptopsAsync(string ABrand) 
+        {
+            var Data = await FDataBase.Laptops.FromSqlRaw("SELECT * FROM Laptops WHERE Brand = '" + ABrand + "'").ToListAsync();
+            return Data;
+        }
+
+    }
+
+}
